86b5edcbc42c04c78207c78068a9a59db30564aa
caye <caye1300@gmail.com>
2020-08-11 11:10:09 -0700

    inserted login storyboard

--- lhsplanner/Controllers/LoginViewController.swift
+++ lhsplanner/Controllers/LoginViewController.swift
@@ -2,11 +2,16 @@
 //  LoginViewController.swift
 //  lhsplanner
 //
-//  Created by Ck2 Jedi on 8/10/20.
+//  Created by Caye on 8/10/20.
 //  Copyright © 2020 caye. All rights reserved.
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
     override func viewDidLoad() {
@@ -15,5 +20,28 @@ class LoginViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let user = authDataResult?.user
+            else { return }
+        
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userDict = snapshot.value as? [String : Any] {
+                print("User already exists \(userDict.debugDescription).")
+            } else {
+                print("New user!")
+            }
+        })
     }
 }
