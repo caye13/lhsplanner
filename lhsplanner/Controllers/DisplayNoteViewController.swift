d8b2da000019106103dcb86c61cd358ed9cf5c11
caye <caye1300@gmail.com>
2020-08-13 10:49:20 -0700

    notes in main view controller

---
+++ lhsplanner/Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayNoteViewController.swift
+//  lhsplanner
+//
+//  Created by Caye on 8/13/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ lhsplanner/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ListNotesTableViewController.swift
+//  lhsplanner
+//
+//  Created by Caye on 8/13/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
