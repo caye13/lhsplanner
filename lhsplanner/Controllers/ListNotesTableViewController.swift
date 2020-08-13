1df1f0f6d54abd6df2b09482808b5029b2a31abe
caye <caye1300@gmail.com>
2020-08-13 11:34:15 -0700

    notes

--- lhsplanner/Controllers/ListNotesTableViewController.swift
+++ lhsplanner/Controllers/ListNotesTableViewController.swift
@@ -12,4 +12,15 @@ class ListNotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+
+        return cell
+    }
 }
