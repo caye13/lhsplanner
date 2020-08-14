6fde232ed608041033fead348072e6dc4010ecd1
caye <caye1300@gmail.com>
2020-08-14 09:30:26 -0700

    coredata for user generated data

--- lhsplanner/Helpers/CoreDataHelper.swift
+++ lhsplanner/Helpers/CoreDataHelper.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 caye. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+struct CoreDataHelper {
+    static let context: NSManagedObjectContext = {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+
+        return context
+    }()
+}
