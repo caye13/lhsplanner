7c0c336158ea039f6402a643a76f24525d3e15af
caye <caye1300@gmail.com>
2020-08-16 13:59:37 -0700

    new feature for initial tableview

---
+++ lhsplanner/Services/CompletedService.swift
@@ -0,0 +1,16 @@
+//
+//  CompletedService.swift
+//  lhsplanner
+//
+//  Created by Caye on 8/16/20.
+//  Copyright © 2020 caye. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+//struct CompletedService {
+//    static func create() {
+//
+//    }
+//}
