b12f6a7d9fded00fe65ec18edf868d4d27584795
caye <caye1300@gmail.com>
2020-08-17 22:11:12 -0700

    complete feature

--- lhsplanner/Controllers/ListNotesTableViewController.swift
+++ lhsplanner/Controllers/ListNotesTableViewController.swift
@@ -45,7 +45,7 @@ class ListNotesTableViewController: UITableViewController {
        let note = notes[indexPath.row]
        cell.noteTitleLabel.text = note.title
        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString() ?? "unknown"
-//    cell.completeButtonPushed = note.completedButton
+//    cell.completeButtonPushed() = note.completedButton
 
        return cell
    }
